3474da4152cd7218fd176a6d8f9dcfa97f84e8aa
pick7110 <pick7110@gmail.com>
2020-11-11 01:18:50 +0900

    初回コミット

---
+++ plugins/response.py
@@ -0,0 +1,24 @@
+import random
+from slackbot.bot import respond_to
+from slackbot.bot import listen_to
+from slackbot.bot import default_reply
+
+
+@respond_to("こんにちは")
+def greeting_1(message):
+    message.reply('こんにちは！！')
+
+@listen_to("コニチワ")
+def greeting_2(message):
+    message.reply('コニチワ！！')
+
+@respond_to("ping")
+def response_3(message):
+    message.reply('pong!')
+
+@respond_to("roulette")
+def response_1(message):
+    res_list = ['山田','川田','スズキ']
+    message.reply(random.choice(res_list))
+    
+    
